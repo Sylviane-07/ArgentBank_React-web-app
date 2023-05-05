d6f2356f4eafed03dda40aa470b6b80a6bad1f6e
Sylviane Farooq <sylvianepinto@hotmail.fr>
2023-05-06 00:22:34 +0200

    User Profile page display & styles

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+//syles
+import styles from "./Profile.module.css";
+//components
+import UserWelcome from "../../components/UserWelcome/UserWelcome";
+//layouts
+import Accounts from "../../layouts/Accounts/Accounts";
 
 function Profile() {
-    return (
-        <div>Profile</div>
-    )
+  return (
+    <main className={`${styles.main} ${styles.bgDark}`}>
+      <UserWelcome />
+      <Accounts />
+    </main>
+  );
 }
 
-export default Profile
+export default Profile;
