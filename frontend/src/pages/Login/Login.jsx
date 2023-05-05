9e6a04ebeb5f5e80efa42f8cacc2fb53c076d944
Sylviane Farooq <sylvianepinto@hotmail.fr>
2023-05-05 22:35:15 +0200

    add Login page display & styles

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+//assets
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+//Styles
+import styles from "./Login.module.css";
+//Components
+import Form from "../../components/Form/Form"
 
 function Login() {
-    return (
-        <div>Login</div>
-    )
+  return (
+    <main className={`${styles.main} ${styles.bgDark}`}>
+      <section className={styles.signInContent}>
+        <FontAwesomeIcon
+          aria-hidden="true"
+          className={styles.signInIcon}
+          icon={faUserCircle}
+        />
+        <h1>Sign In</h1>
+        <Form/>
+      </section>
+    </main>
+  );
 }
 
-export default Login
+export default Login;
