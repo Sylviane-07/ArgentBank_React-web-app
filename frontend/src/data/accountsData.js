c99ac46709bd387b8492c313ff326fe22829f375
Sylviane Farooq <sylvianepinto@hotmail.fr>
2023-05-06 00:21:41 +0200

    create account details data.js file

---
+++ frontend/src/data/accountsData.js
@@ -0,0 +1,22 @@
+const accountsDetailsData = [
+  {
+    id: "64543052cf43d67f78760c24",
+    title: "Argent Bank Checking (x8349)",
+    amount: "$2,082.79",
+    status: "Available Balance",
+  },
+  {
+    id: "64543052ae8da4c989dd441e",
+    title: "Argent Bank Savings (x6712)",
+    amount: "$10,928.42",
+    status: "Available Balance",
+  },
+  {
+    id: "645430525b62f33f70484484",
+    title: "Argent Bank Credit Card (x8349)",
+    amount: "$184.30",
+    status: "Current Balance",
+  },
+];
+
+export default accountsDetailsData;
