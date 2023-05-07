be2ffc8b8f080bb62c0c3b60e9d993585ff684c2
Sylviane Farooq <sylvianepinto@hotmail.fr>
2023-05-07 22:35:04 +0200

    add Redux ToolKit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 //Styles
 import "./index.css";
+//Redux
+import { store } from "./redux/store"
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
