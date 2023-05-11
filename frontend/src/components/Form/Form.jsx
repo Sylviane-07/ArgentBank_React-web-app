38cc1a16249b2bc8314e1b6576bf41ffa9f0770d
Sylviane Farooq <sylvianepinto@hotmail.fr>
2023-05-11 23:41:26 +0200

    check token in form and navigate to profile

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 //Styles
 import styles from "./Form.module.css";
 //components
@@ -8,16 +8,15 @@ import Button from "../../components/Button/Button";
 import { useUserLoginMutation } from "../../redux/features/apiSlice";
 
 function Form() {
-
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
   const { email, password } = formData;
-  const [userLogin, { isLoading, isSuccess, isError, error }] =
+  const [userLogin, { data, isLoading, isSuccess, isError, error }] =
     useUserLoginMutation();
-  
-  const navigate = useNavigate()
+
+  const navigate = useNavigate();
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -29,20 +28,27 @@ function Form() {
   const onSubmit = (e) => {
     e.preventDefault();
     //Query to set userToken to localStorage
-    userLogin({email, password});
-    //Reset form imputs   
+    userLogin({ email, password });
+    //Reset form imputs
     setFormData({
       email: "",
       password: "",
     });
   };
- 
+
   useEffect(() => {
-    //userLogin side effect => navigate to profil if logged-in  
+    //userLogin side effect => navigate to profil if logged-in
     if (isSuccess && localStorage.getItem("accessToken")) {
-      navigate("/profile");
+      //Checking if stored token is === to response token
+      const userToken = JSON.parse(localStorage.getItem("accessToken"));
+      const responseToken = data?.body?.token;
+      // console.log(responseToken);
+      // console.log(userToken);
+      if (userToken === responseToken) {
+        navigate("/profile");
+      }
     }
-  }, [isSuccess, navigate]);
+  }, [isSuccess, data, navigate]);
 
   return (
     <form onSubmit={onSubmit} id="loginForm">
@@ -72,7 +78,11 @@ function Form() {
         <input type="checkbox" id="remember-me" />
         <label htmlFor="remember-me">Remember me</label>
       </div>
-      <Button type={"submit"} btnText={"Sign In"} className={styles.signInBtn} />
+      <Button
+        type={"submit"}
+        btnText={"Sign In"}
+        className={styles.signInBtn}
+      />
       {isLoading && <p>Loading...</p>}
       {isError && <p>{error?.data?.message}</p>}
     </form>
--- frontend/src/redux/features/apiSlice.js
+++ frontend/src/redux/features/apiSlice.js
@@ -1,10 +1,11 @@
 //Import Redux ToolKit (RTK) Query
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
 const baseUrl = "http://localhost:3001/api/v1/";
 
 export const apiSlice = createApi({
-  reducerPath: "api",
+  reducerPath: "apiSlice",
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     userLogin: builder.mutation({
@@ -13,15 +14,14 @@ export const apiSlice = createApi({
         method: "POST",
         body: { email, password },
       }),
-        transformResponse: (response) => {
-            const userToken = response.body.token
-          //Store user token in localStorage
-          localStorage.setItem("accessToken", JSON.stringify(userToken));
-          return response
-        }
+      transformResponse: (response) => {
+        const responseToken = response.body.token;
+        //Store userToken in localStorage
+        localStorage.setItem("accessToken", JSON.stringify(responseToken));
+        return response;
+      },
     }),
   }),
 });
-
 
 export const { useUserLoginMutation } = apiSlice;
